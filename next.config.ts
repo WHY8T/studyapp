b1b575718253480f992721976fb9ad5695e9b850
abdelwahebghoubali <abdelwahebghoubali@gmail.com>
2026-04-16 23:02:29 +0200

    fix: turbopack config and rename middleware to proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
@@ -17,9 +17,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+export default nextConfig;
